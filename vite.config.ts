ce3d5357cbfecc0cc96ffc19890532483f470f10
Ayomide Akinola <drayfocus@gmail.com>
2025-04-23 17:24:07 +0100

    feat: prepare for web

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/POSMobileApp/",
+  base: "/POSMobileApp/",
   plugins: [vue(), legacy(), tailwindcss()],
   resolve: {
     alias: {
