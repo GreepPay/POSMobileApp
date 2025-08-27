588418ebb63f55e4cf12ecb6ce4599ce77f4d110
Daniel Ufeli <danielufeli@yahoo.com>
2025-08-27 08:27:10 +0100

    fix

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -121,9 +121,105 @@ export default defineComponent({
         try {
           await Logic.Auth.SignIn(true);
           await Logic.Auth.GetAuthUser();
+          loadingState.value = false;
 
-          handleAuthResponse(formData);
-        } finally {
+          const authUser: User = Logic.Auth.AuthUser;
+
+          // Check if user has a profile set
+          if (!authUser?.email_verified_at) {
+            Logic.Auth.ResendEmailOTP(
+              Logic.Auth.AuthUser?.email || localStorage.getItem("auth_email")
+            );
+            // Save auth email and pass
+            localStorage.setItem(
+              "auth_email",
+              Logic.Auth.SignInForm?.email || ""
+            );
+            localStorage.setItem(
+              "auth_pass",
+              Logic.Auth.SignInForm?.password || ""
+            );
+            Logic.Common.GoToRoute("/auth/verify-email");
+            return;
+          }
+
+          // Check if user has a profile set
+          if (!Logic.Auth.AuthUser?.first_name) {
+            // Save auth email and pass
+            localStorage.setItem(
+              "auth_email",
+              Logic.Auth.SignInForm?.email || ""
+            );
+            localStorage.setItem(
+              "auth_pass",
+              Logic.Auth.SignInForm?.password || ""
+            );
+            Logic.Common.GoToRoute("/auth/setup-account");
+            return;
+          }
+
+          if (authUser.businesses?.length == 0) {
+            // Save auth email and pass
+            localStorage.setItem(
+              "auth_email",
+              Logic.Auth.SignInForm?.email || ""
+            );
+            localStorage.setItem(
+              "auth_pass",
+              Logic.Auth.SignInForm?.password || ""
+            );
+            Logic.Common.GoToRoute(`/auth/setup`);
+            return;
+          } else {
+            const businesses = authUser.businesses;
+            if (
+              businesses &&
+              businesses.some((business) => business.wallet === null)
+            ) {
+              const businessWithoutWallet = businesses.find(
+                (item) => !item.wallet
+              );
+              Logic.Common.GoToRoute(
+                `/auth/setup?business=${businessWithoutWallet?.id}`
+              );
+              return;
+            }
+          }
+
+          if (authUser.transaction_pin) {
+            const authLoginData = {
+              email: formData.email,
+              password: formData.password,
+            };
+
+            // Encrypt data
+            const encryptedData = Logic.Common.encryptData(
+              authLoginData,
+              authUser.transaction_pin
+            );
+
+            localStorage.setItem("auth_encrypted_data", encryptedData);
+            localStorage.setItem("auth_passcode", authUser.transaction_pin);
+          }
+
+          // Check if passcode has been set
+          if (localStorage.getItem("auth_passcode")) {
+            Logic.Common.GoToRoute("/", true);
+            return;
+          } else {
+            // Save auth email and pass
+            localStorage.setItem(
+              "auth_email",
+              Logic.Auth.SignInForm?.email || ""
+            );
+            localStorage.setItem(
+              "auth_pass",
+              Logic.Auth.SignInForm?.password || ""
+            );
+            Logic.Common.GoToRoute("/auth/set-passcode");
+            return;
+          }
+        } catch {
           loadingState.value = false;
         }
       } else {
