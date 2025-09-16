875638841254877aac8cd1fb6277047faba38321
Pluri45 <snipertft45@gmail.com>
2025-09-16 23:14:04 +0100

    new updates

--- src/views/auth/new-password.vue
+++ src/views/auth/new-password.vue
@@ -6,50 +6,48 @@
         :parent-refs="parentRefs"
         class="w-full flex flex-col pt-2"
       >
-        <app-normal-text custom-class="!text-center !text-gray-600 !mb-3">
+        <app-normal-text  
+          custom-class="!text-center !text-gray-600 !text-sm !mb-6"
+        >
           Must be at least 8 characters.
         </app-normal-text>
-
-        <div class="w-full flex flex-col pt-4 px-4">
-          <div class="flex flex-col pb-3">
-            <app-text-field
-              v-model="formData.password"
-              :has-title="true"
-              type="password"
-              placeholder="Enter new password"
-              ref="passwordRef"
-              name="Password"
-              use-floating-label
-              :rules="[FormValidations.RequiredRule]"
-              custom-class="!border-gray-300 focus:!border-primary"
-            />
-          </div>
-          <div class="flex flex-col pb-3">
-            <app-text-field
-              v-model="formData.confirm_password"
-              :has-title="true"
-              type="password"
-              placeholder="Enter new password again"
-              ref="confirm_password"
-              name="Confirm Password"
-              use-floating-label
-              :rules="[
-                FormValidations.RequiredRule,
-                FormValidations.handleConfirmPassword(
-                  formData.password,
-                  formData.confirm_password
-                ),
-              ]"
-              custom-class="!border-gray-300 focus:!border-primary"
-            />
-          </div>
+        
+        <div class="w-full flex flex-col space-y-4 pt-4 px-4">
+          <app-text-field
+            v-model="formData.password"
+            :has-title="true"
+            type="password"
+            placeholder="Enter new password"
+            ref="passwordRef"
+            name="Password"
+            use-floating-label
+            :rules="[FormValidations.RequiredRule]"
+            custom-class="!border-gray-300 focus:!border-primary"
+          />
+          <app-text-field
+            v-model="formData.confirm_password"
+            :has-title="true"
+            type="password"
+            placeholder="Enter new password again"
+            ref="confirm_password"
+            name="Confirm Password"
+            use-floating-label
+            :rules="[
+              FormValidations.RequiredRule,
+              FormValidations.handleConfirmPassword(
+                formData.password,
+                formData.confirm_password
+              ),
+            ]"
+            custom-class="!border-gray-300 focus:!border-primary"
+          />
         </div>
-
+        
         <!-- Button -->
-        <div class="w-full pt-5 px-4">
+          <div class="w-full pt-5 px-4">
           <app-button
             variant="secondary"
-            class="w-full py-4"
+            class="w-full py-4 "
             @click.prevent="handlePassword"
           >
             Create Password
@@ -59,6 +57,7 @@
     </subpage-layout>
   </app-wrapper>
 </template>
+
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from "vue";
@@ -71,7 +70,7 @@ import {
 import { Logic } from "@greep/logic";
 
 export default defineComponent({
-  name: "NewSetPassWordpage",
+  name: "NewPassWordpage",
   components: {
     AppNormalText,
     AppFormWrapper,
@@ -80,7 +79,7 @@ export default defineComponent({
   },
   setup() {
     const FormValidations = Logic.Form;
-    const loadingState = ref(false);
+    const loadingState = ref(false)
     const formComponent = ref<any>(null);
     const formData = reactive({
       password: "",
@@ -94,7 +93,7 @@ export default defineComponent({
         try {
           const otp = localStorage.getItem("code");
           const uuid = localStorage.getItem("reset_password_uuid");
-
+          
           if (!otp) {
             Logic.Common.showAlert({
               show: true,
@@ -104,28 +103,19 @@ export default defineComponent({
             return;
           }
 
-          loadingState.value = true;
-
-          Logic.Auth.ResetPasswordForm = {
-            newPassword: formData.password,
-            otpCode: otp,
-            userUuid: uuid || "",
-          };
-
-          Logic.Common.showLoader({
-            show: true,
-            loading: true
-          })
+         loadingState.value = true;
 
           // Send the reset password request
-          await Logic.Auth.ResetPassword();
-
-          Logic.Common.hideLoader();
+          await Logic.Auth.ResetPassword({
+            new_password: formData.password,
+            otp_code: otp,
+            user_uuid: uuid || "",
+          });
 
           // Clear the OTP from local storage
           localStorage.removeItem("code");
           localStorage.removeItem("reset_password_uuid");
-
+          
           Logic.Common.showAlert({
             show: true,
             message: "Password reset successfully!",
@@ -134,6 +124,8 @@ export default defineComponent({
 
           // Redirect to login or success page
           Logic.Common.GoToRoute("/auth/login"); // or your desired route
+
+
         } catch (error) {
           Logic.Common.showAlert({
             show: true,
