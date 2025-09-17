4333c67060b4086ec51bda4adf6d1b6608c823f3
Dasaolu Daniel <yungdansa@gmail.com>
2025-09-17 01:47:56 +0100

    ...

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
       v-if="showUpdateAppModal"
       :close="
         () => {
-          showUpdateAppModal = false;
+          showUpdateAppModal = false
         }
       "
       :contentClass="'!px-0'"
@@ -57,276 +57,274 @@
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet, isPlatform } from "@ionic/vue";
-import { defineComponent, onMounted, ref, watch } from "vue";
-import { App as CapacitorApp, URLOpenListenerEvent } from "@capacitor/app";
-import { getPlatforms } from "@ionic/vue";
-import { useRoute, useRouter } from "vue-router";
-import { Logic } from "@greep/logic";
-import { registerSW } from "virtual:pwa-register";
-import {
-  SetFrontendLogic,
-  AppAlert,
-  AppLoader,
-  AppBottomBar,
-  AppModal,
-  AppNormalText,
-  AppButton,
-} from "@greep/ui-components";
-import { SplashScreen } from "@capacitor/splash-screen";
-import { reactive } from "vue";
-import { Business } from "@greep/logic/src/gql/graphql";
-import { getBottomPadding, showUpdateAppModal } from "./composable";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    IonApp,
-    IonRouterOutlet,
+  import { IonApp, IonRouterOutlet, isPlatform } from "@ionic/vue"
+  import { defineComponent, onMounted, ref, watch } from "vue"
+  import { App as CapacitorApp, URLOpenListenerEvent } from "@capacitor/app"
+  import { getPlatforms } from "@ionic/vue"
+  import { useRoute, useRouter } from "vue-router"
+  import { Logic } from "@greep/logic"
+  import { registerSW } from "virtual:pwa-register"
+  import {
+    SetFrontendLogic,
     AppAlert,
     AppLoader,
     AppBottomBar,
     AppModal,
     AppNormalText,
     AppButton,
-  },
-  setup() {
-    const router: any = useRouter();
-    const route = useRoute();
+  } from "@greep/ui-components"
+  import { SplashScreen } from "@capacitor/splash-screen"
+  import { reactive } from "vue"
+  import { Business } from "@greep/logic/src/gql/graphql"
+  import { getBottomPadding, showUpdateAppModal } from "./composable"
 
-    const alertSetup = ref(Logic.Common.alertSetup);
-    const loaderSetup = ref(Logic.Common.loaderSetup);
-    const showBottomNav = ref<any>();
-    const AuthUser = ref(Logic.Auth.AuthUser);
+  export default defineComponent({
+    name: "App",
+    components: {
+      IonApp,
+      IonRouterOutlet,
+      AppAlert,
+      AppLoader,
+      AppBottomBar,
+      AppModal,
+      AppNormalText,
+      AppButton,
+    },
+    setup() {
+      const router: any = useRouter()
+      const route = useRoute()
 
-    const selectedTab = ref("");
+      const alertSetup = ref(Logic.Common.alertSetup)
+      const loaderSetup = ref(Logic.Common.loaderSetup)
+      const showBottomNav = ref<any>()
+      const AuthUser = ref(Logic.Auth.AuthUser)
 
-    // Set routers
-    Logic.Common.SetRouter(router);
-    Logic.Common.SetRoute(route);
+      const selectedTab = ref("")
 
-    // Set UI frontend logic
-    SetFrontendLogic(Logic);
+      // Set routers
+      Logic.Common.SetRouter(router)
+      Logic.Common.SetRoute(route)
 
-    const appVersion = ref("1.10");
+      // Set UI frontend logic
+      SetFrontendLogic(Logic)
 
-    // Set app version
-    localStorage.setItem("app_version", appVersion.value);
+      const appVersion = ref("1.10")
 
-    const bottomBar = reactive([
-      {
-        path: "/",
-        icon: "home",
-        routeTag: "base",
-        name: "Home",
-      },
-    ]);
+      // Set app version
+      localStorage.setItem("app_version", appVersion.value)
 
-    // Set app url
+      const bottomBar = reactive([
+        {
+          path: "/",
+          icon: "home",
+          routeTag: "base",
+          name: "Home",
+        },
+      ])
 
-    const setBottomBar = () => {
-      bottomBar.length = 0;
-      bottomBar.push({
-        path: "/",
-        icon: "home",
-        routeTag: "base",
-        name: "Home",
-      });
-      // Set bottom nav
-      if (Logic.Auth.AuthUser) {
-        const business: Business = Logic.Auth.GetDefaultBusiness();
+      // Set app url
 
-        if (business?.business_type == "exchanger") {
-          bottomBar.push(
-            ...[
-              {
-                path: "/p2p",
-                icon: "ads",
-                routeTag: "p2p",
-                name: "P2P",
-              },
-              {
-                path: "/orders",
-                icon: "order",
-                routeTag: "orders",
-                name: "Orders",
-              },
-            ]
-          );
-        }
+      const setBottomBar = () => {
+        bottomBar.length = 0
+        bottomBar.push({
+          path: "/",
+          icon: "home",
+          routeTag: "base",
+          name: "Home",
+        })
+        // Set bottom nav
+        if (Logic.Auth.AuthUser) {
+          const business: Business = Logic.Auth.GetDefaultBusiness()
 
-        if (business?.business_type == "event_host") {
-          bottomBar.push(
-            ...[
-              {
-                path: "/events",
-                icon: "events",
-                routeTag: "events",
-                name: "Events",
-              },
-              {
-                path: "/orders",
-                icon: "order",
-                routeTag: "orders",
-                name: "Orders",
-              },
-            ]
-          );
+          if (business?.business_type == "exchanger") {
+            bottomBar.push(
+              ...[
+                {
+                  path: "/p2p",
+                  icon: "ads",
+                  routeTag: "p2p",
+                  name: "P2P",
+                },
+                {
+                  path: "/orders",
+                  icon: "order",
+                  routeTag: "orders",
+                  name: "Orders",
+                },
+              ]
+            )
+          }
+
+          if (business?.business_type == "event_host") {
+            bottomBar.push(
+              ...[
+                {
+                  path: "/events",
+                  icon: "events",
+                  routeTag: "events",
+                  name: "Events",
+                },
+                {
+                  path: "/orders",
+                  icon: "order",
+                  routeTag: "orders",
+                  name: "Orders",
+                },
+              ]
+            )
+          }
         }
       }
-    };
 
-    Logic.Common.SetApiUrl(
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      // (import.meta as any).env.VITE_API_URL ?? "http://localhost:3000/graphql"
-      (import.meta as any).env.VITE_API_URL ??
-        "https://api-pos-dev.greep.io/graphql"
-    );
+      Logic.Common.SetApiUrl(
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        (import.meta as any).env.VITE_API_URL ?? "http://localhost:3000/graphql" 
+      )
 
-    const handleMountActions = () => {
-       const authToken = Logic.Auth.AccessToken;
+      const handleMountActions = () => {
+        const authToken = Logic.Auth.AccessToken
 
-      // If user is authenticated
-      if (authToken) {
-        Logic.Common.initiateWebSocket({
-          pusherKey: import.meta.env.VITE_PUSHER_APP_KEY,
-          pusherHost: import.meta.env.VITE_PUSHER_HOST,
-          pusherPort: import.meta.env.VITE_PUSHER_PORT,
-          pusherCluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-          socketAuthUrl: import.meta.env.VITE_SOCKET_AUTH_URL,
-        });
-        Logic.Auth.GetAuthUser();
+        // If user is authenticated
+        if (authToken) {
+          Logic.Common.initiateWebSocket({
+            pusherKey: import.meta.env.VITE_PUSHER_APP_KEY,
+            pusherHost: import.meta.env.VITE_PUSHER_HOST,
+            pusherPort: import.meta.env.VITE_PUSHER_PORT,
+            pusherCluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+            socketAuthUrl: import.meta.env.VITE_SOCKET_AUTH_URL,
+          })
+          Logic.Auth.GetAuthUser()
 
-        const pathContainsIsForceReload =
-          window.location.search.includes("isForceReload");
+          const pathContainsIsForceReload =
+            window.location.search.includes("isForceReload")
 
-        if(!AuthUser.value?.first_name) {
-          Logic.Common.GoToRoute("/auth/setup-account");
-          return;
-        }
-
-        if((AuthUser.value?.businesses?.length || 0) == 0) {
-          Logic.Common.GoToRoute("/auth/setup");
-          return;
-        }
-
-        if (
-          localStorage.getItem("auth_passcode") &&
-          localStorage.getItem("auth_encrypted_data") &&
-          !pathContainsIsForceReload
-        ) {
-          Logic.Common.GoToRoute("/auth/welcome");
-        }
-      } else {
-        localStorage.clear();
-        // Set app version
-        localStorage.setItem("app_version", appVersion.value);
-        // Go to start page
-        // Only if the path does not contain /auth
-        if (!window.location.pathname.includes("/auth")) {
-          const isPWA = isPlatform("pwa");
-          console.log("IsPWA", isPWA);
-          if (!isPWA) {
-            Logic.Common.GoToRoute("/start/pwa");
-            return;
+          if (!AuthUser.value?.first_name) {
+            Logic.Common.GoToRoute("/auth/setup-account")
+            return
           }
-          Logic.Common.GoToRoute("/start");
+
+          if ((AuthUser.value?.businesses?.length || 0) == 0) {
+            Logic.Common.GoToRoute("/auth/setup")
+            return
+          }
+
+          if (
+            localStorage.getItem("auth_passcode") &&
+            localStorage.getItem("auth_encrypted_data") &&
+            !pathContainsIsForceReload
+          ) {
+            Logic.Common.GoToRoute("/auth/welcome")
+          }
         } else {
-          return Logic.Common.GoToRoute("/auth/login");
-        }
-      }
-    };
-
-    const updateSW = registerSW({
-      onNeedRefresh() {
-        // Instead of prompting user, just reload
-        showUpdateAppModal.value = true;
-      },
-      onOfflineReady() {
-        console.log("App ready to work offline");
-      },
-    });
-
-    const updateApp = () => {
-      showUpdateAppModal.value = false;
-      updateSW(true); // activates new SW
-      window.location.reload();
-    };
-
-    watch(AuthUser, () => {
-      setBottomBar();
-    });
-
-    onMounted(async () => {
-      // Logic.Common.GoToRoute("/auth/login");
-      // deep link config
-
-      // Logic.Auth.GetCurrentAppVersion()?.then((version) => {
-      //   checkAppVersion(version || "1.0");
-      // });
-
-      CapacitorApp.addListener(
-        "appUrlOpen",
-        function (event: URLOpenListenerEvent) {
-          // Example url: https://beerswift.app/tabs/tabs2
-          // slug = /tabs/tabs2
-          const domainType = ".com";
-          const slug = event.url.split(domainType).pop();
-
-          // We only push to the route if there is a slug present
-          if (slug) {
-            if (getPlatforms()[0] == "android") {
-              window.location.href = `https://localhost${slug}`;
-              return;
+          localStorage.clear()
+          // Set app version
+          localStorage.setItem("app_version", appVersion.value)
+          // Go to start page
+          // Only if the path does not contain /auth
+          if (!window.location.pathname.includes("/auth")) {
+            const isPWA = isPlatform("pwa")
+            console.log("IsPWA", isPWA)
+            if (!isPWA) {
+              Logic.Common.GoToRoute("/start/pwa")
+              return
             }
-            Logic.Common.GoToRoute(slug);
-            return;
+            Logic.Common.GoToRoute("/start")
+          } else {
+            return Logic.Common.GoToRoute("/auth/login")
           }
         }
-      );
-
-      handleMountActions();
-
-      // Register watchers
-      Logic.Common.watchProperty("alertSetup", alertSetup);
-      Logic.Common.watchProperty("loaderSetup", loaderSetup);
-      Logic.Common.watchProperty("showBottomNav", showBottomNav);
-      Logic.Auth.watchProperty("AuthUser", AuthUser);
-
-      await SplashScreen.show({
-        showDuration: 3000,
-        autoHide: true,
-      });
-    });
-
-    const tabIsActive = (tabName: string) => {
-      const mainName = tabName;
-
-      if (mainName == "base" && router.currentRoute.value.path == "/") {
-        return true;
-      } else if (
-        mainName != "base" &&
-        router.currentRoute.value.path.includes(mainName)
-      ) {
-        selectedTab.value = mainName;
-        return true;
       }
 
-      return false;
-    };
+      const updateSW = registerSW({
+        onNeedRefresh() {
+          // Instead of prompting user, just reload
+          showUpdateAppModal.value = true
+        },
+        onOfflineReady() {
+          console.log("App ready to work offline")
+        },
+      })
 
-    return {
-      alertSetup,
-      loaderSetup,
-      Logic,
-      tabIsActive,
-      showBottomNav,
-      bottomBar,
-      showUpdateAppModal,
-      getBottomPadding,
-      updateApp,
-    };
-  },
-});
+      const updateApp = () => {
+        showUpdateAppModal.value = false
+        updateSW(true) // activates new SW
+        window.location.reload()
+      }
+
+      watch(AuthUser, () => {
+        setBottomBar()
+      })
+
+      onMounted(async () => {
+        // Logic.Common.GoToRoute("/auth/login");
+        // deep link config
+
+        // Logic.Auth.GetCurrentAppVersion()?.then((version) => {
+        //   checkAppVersion(version || "1.0");
+        // });
+
+        CapacitorApp.addListener(
+          "appUrlOpen",
+          function (event: URLOpenListenerEvent) {
+            // Example url: https://beerswift.app/tabs/tabs2
+            // slug = /tabs/tabs2
+            const domainType = ".com"
+            const slug = event.url.split(domainType).pop()
+
+            // We only push to the route if there is a slug present
+            if (slug) {
+              if (getPlatforms()[0] == "android") {
+                window.location.href = `https://localhost${slug}`
+                return
+              }
+              Logic.Common.GoToRoute(slug)
+              return
+            }
+          }
+        )
+
+        handleMountActions()
+
+        // Register watchers
+        Logic.Common.watchProperty("alertSetup", alertSetup)
+        Logic.Common.watchProperty("loaderSetup", loaderSetup)
+        Logic.Common.watchProperty("showBottomNav", showBottomNav)
+        Logic.Auth.watchProperty("AuthUser", AuthUser)
+
+        await SplashScreen.show({
+          showDuration: 3000,
+          autoHide: true,
+        })
+      })
+
+      const tabIsActive = (tabName: string) => {
+        const mainName = tabName
+
+        if (mainName == "base" && router.currentRoute.value.path == "/") {
+          return true
+        } else if (
+          mainName != "base" &&
+          router.currentRoute.value.path.includes(mainName)
+        ) {
+          selectedTab.value = mainName
+          return true
+        }
+
+        return false
+      }
+
+      return {
+        alertSetup,
+        loaderSetup,
+        Logic,
+        tabIsActive,
+        showBottomNav,
+        bottomBar,
+        showUpdateAppModal,
+        getBottomPadding,
+        updateApp,
+      }
+    },
+  })
 </script>
