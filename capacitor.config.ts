054f8669b9d481ddbbbd01f026e5dc96386f5c73
Ayomide <ayomide@ceedlab.co>
2025-11-08 15:15:05 +0100

    fix: update products

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -25,7 +25,7 @@ const config: CapacitorConfig = {
       providers: ["google.com", "apple.com"],
     },
     Keyboard: {
-      resizeOnFullScreen: true,
+      resizeOnFullScreen: false,
     },
   },
 };
--- dist/assets/web-CI_eDrnM.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-CjVQ28Ba.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
